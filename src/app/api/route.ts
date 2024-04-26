2ef95b6e8770d8bf6edbedbb4fa40f7612939b72
Sim Yee Xing <yeexing@whiteroom.work>
2024-04-26 16:00:45 +0800

    add api route

---
+++ src/app/api/route.ts
@@ -0,0 +1,34 @@
+// app/api/route.js 👈🏽
+import { WebflowClient, Webflow } from "webflow-api";
+import { NextResponse } from "next/server";
+
+const webflow = new WebflowClient({
+  accessToken: process.env.API_TOKEN ?? "",
+});
+
+// REFER: https://stackoverflow.com/a/75418737
+// To handle a GET request to /api
+export async function GET(request: any) {
+  // Do whatever you want
+
+  if (!process.env.SITE_ID) {
+    return NextResponse.json({ message: "Site ID not found" }, { status: 400 });
+  }
+  try {
+    const collections = await webflow.collections.list(process.env.SITE_ID);
+    console.log(process.env.SITE_ID);
+    console.log(collections);
+  } catch (e) {
+    console.log(e);
+  }
+
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// To handle a POST request to /api
+export async function POST(request: any) {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// Same logic to add a `PATCH`, `DELETE`...
