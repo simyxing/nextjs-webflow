bd4e5eb31925a208c4911e100d7a6e00f1229642
Yee Xing <yeexing@whiteroom.work>
2024-04-28 23:35:58 +0800

    handle add item api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -31,20 +31,48 @@ export async function GET(request: any) {
       options
     );
 
-   const parsedData = await data.json()
+    const parsedData = await data.json();
 
-    return NextResponse.json(
-      parsedData,
-      { status: 400 }
-    );  } catch (e) {
+    return NextResponse.json(parsedData, { status: 400 });
+  } catch (e) {
     return NextResponse.json({ status: 500 });
   }
 }
 
 // To handle a POST request to /api
 export async function POST(request: any) {
-  // Do whatever you want
-  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+  const data = await request.json();
+
+  try {
+    const url =
+      "https://api.webflow.com/v2/collections/662b431bea4123556ea003ee/items";
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        Authorization: "Bearer " + process.env.API_TOKEN,
+      },
+      body: JSON.stringify({
+        isArchived: false,
+        isDraft: false,
+        fieldData: {
+          name: data.bookName,
+          author: data.bookAuthor,
+        },
+      }),
+    };
+
+    const res = await fetch(url, options);
+    return NextResponse.json({ message: "New item created" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    // Do whatever you want
+    return NextResponse.json(
+      { message: "Error creating new item" },
+      { status: 500 }
+    );
+  }
 }
 
 // Same logic to add a `PATCH`, `DELETE`...
--- src/app/~/books/page.tsx
+++ src/app/~/books/page.tsx
@@ -1,76 +1,81 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+import { FormEvent, useState } from "react";
 
 const fetchBooks = async () => {
   try {
-    const res = await fetch("/api");
-    // return await res.json();
+    const data = await fetch(`/api`);
 
-
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: "Bearer " + process.env.API_TOKEN,
-      },
-    };
-
-    const data = await fetch(
-      `/api`
-    );
-
-    // console.log("a");
-    // console.log("a");
-    // console.log("a");
-    // console.log("a");
-    // console.log("a");
-    // console.log(data, "data hereee");
-    // // console.log(data.json());
-    // console.log("a");
-    // console.log("a");
-    // console.log("a");
-    // console.log("a");
-    // console.log("a");
-    // console.log("a");
-
-    // const collections = await webflow.collections.list(process.env.SITE_ID);
-    // console.log(process.env.SITE_ID);
-    // console.log(collections);
-    // return NextResponse.json(
-    //   { message: "collection:" + collections.collections?.length },
-    //   { status: 400 }
-    // );
-   return await data.json()
-
+    return await data.json();
   } catch (error) {
-    throw new Error ('Failed to get data')
+    throw new Error("Failed to get data");
   }
 };
 
 export default function Page() {
   //https://medium.com/@ecarina.gonzalez/react-query-integration-with-next-js-are-you-ready-7433568356f2
-  const { data: books, isLoading, isError, error } = useQuery({
+  const {
+    data: books,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryFn: async () => await fetchBooks(),
     queryKey: ["books"],
   });
 
+  const [bookName, setBookName] = useState("");
+  const [bookAuthor, setBookAuthor] = useState("");
+
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
-  console.log(books, error);
+  console.log(bookName, bookAuthor);
+
+  //create
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    await fetch("/api", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        bookName: bookName,
+        bookAuthor: bookAuthor,
+      }),
+    });
+  }
+
   return (
-    <div>
+    <div style={{ padding: "2rem" }}>
       <h1>Books</h1>
-        <div>
-                {books.items.map((book:any) => (
+      <div>
+        {books.items.map((book: any) => (
           <li key={book.id}>
             {book.fieldData.name} by {book.fieldData.author}
           </li>
         ))}
-        </div>
-  
+      </div>
+      <br />
+      <h1>Add Book</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={bookName}
+          onChange={(e) => setBookName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="author"
+          value={bookAuthor}
+          onChange={(e) => setBookAuthor(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
-
-
